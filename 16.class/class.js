b17d6903870ff579d2e656128cfcd62bf5fae1e9
Santhi10p <112076530+Santhi10p@users.noreply.github.com>
2025-08-14 19:00:22 +0530

    Add files via upload
    16.class

---
+++ 16.class/class.js
@@ -0,0 +1,18 @@
+
+class Student{
+
+    sAge;
+    sName;
+
+    constructor(sAge,sName)
+    {
+        this.sAge=sAge;
+        this.sName=sName
+    }
+}
+
+let s1=new Student(31,"hardik")
+
+console.log(s1.sAge);
+console.log(s1.sName);
+
